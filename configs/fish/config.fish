ec517c62c6a75e4fce96b6a72cb53a315a5893af
Ashborn013 <csarjun11@gmail.com>
2023-10-10 18:05:05 +0530

    Tuesday 10 October 2023 06:05:05 PM IST auto backup

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -10,13 +10,26 @@ end
 if [ -d "$HOME/.local/bin" ] ;
   set PATH  "$HOME/.local/bin:$PATH"
 end
+
+# command_timeout 
+
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 #set XDG_DATA_DIRS "/var/lib/flatpak/exports/share:/home/arjun/.local/share/flatpak/exports/share "
 export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
-set BROWSER brave
-######## Alias ########
-set office "/app/onlyoffice/DocumentServer/lib/documentserver-example/files/172.18.0.1/"
+export EDITOR='nvim'
+export DENO_INSTALL="/home/arjun/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
 
+
+set office "/app/onlyoffice/DocumentServer/lib/documentserver-example/files/"
+# set BROWSER firefox
+set BROWSER google-chrome-stable
+
+
+set hubbox "/run/media/arjun/New Volume/Guest OS/VunHub"
+
+######## Alias ########
+#
 alias clean='yay -Rns $(yay -Qdtq)'
 alias sizeof="du -hsx * | sort -rh"
 alias homevol='cd "/run/media/arjun/New Volume"'
@@ -28,13 +41,14 @@ alias vim='nvim'
 alias mirrors="sudo reflector --verbose --latest 5 --country 'India' --age 6 --sort rate --save /etc/pacman.d/mirrorlist"
 alias grub-update="sudo grub-mkconfig -o /boot/grub/grub.cfg"
 alias maintenance="yay -Sc && sudo pacman -Scc"
-alias update="yay -Syu"
 alias musica="ncmpcpp"
 alias msetup='termite -e cava & termite -e ncmpcpp  & musica'
-alias notes='cd "/run/media/arjun/New Volume/Notes/Year 2/4th sem"'
+alias notes='cd "/run/media/arjun/New Volume/Notes/Year 3/5th sem"'
 alias ctf='cd "/run/media/arjun/New Volume/Shared/CTF"'
 alias codedir='cd "/run/media/arjun/New Volume/code and stuff"'
+
 # confirm before overwriting something
+
 alias cp="cp -v"
 alias mv="mv -v"
 alias rm="rm -v"
@@ -43,18 +57,14 @@ alias ls='lsd  --group-directories-first'
 alias ll='lsd -la --group-directories-first'
 alias of='set var  $(fzf) &&  xdg-open $var'
 alias battary='acpi'
-alias nexttail='npx create-next-app@latest --example with-tailwindcss'
 alias imview='kitty +kitten icat'
 alias gitdir='cd "/run/media/arjun/New Volume/code and stuff/git"'
 alias docker='sudo docker '
-#LS_COLORS=$LS_COLORS:'ow=1;34:' ; export LS_COLORS
-#~/.local/bin/colorscript -r
-# Import colorscheme from 'wal' asynchronously
-# &   # Run the process in the background.
-# ( ) # Hide shell job control messages.
-#(cat ~/.cache/wal/sequences &)
 
-# Alternative (blocks terminal for 0-3ms)
 cat ~/.cache/wal/sequences
 
 fish_add_path /home/arjun/.spicetify
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
